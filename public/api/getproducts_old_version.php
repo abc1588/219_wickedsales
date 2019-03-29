115063f7bf53443cd3a15c1ab66261ff2668290f
Caroline Pan <abc1588@yahoo.com>
2019-03-29 16:50:32 -0700

    added endpoint, push images into an array, convert number to JSON

---
+++ public/api/getproducts_old_version.php
@@ -0,0 +1,52 @@
+<?php
+
+//phpinfo();
+
+//file_get_contents('./data/getproducts.json');  //this does not show any data
+//readfile('./data/getproducts.json');
+
+// include     include_once     require
+require_once('mysqlconnect.php');
+
+// query stmt: SELECT * FROM `products`
+//$query = 'SELECT * FROM `products`';
+
+$query = "SELECT p.id, p.name, p.price, i.url as image
+      FROM `products` AS p
+      JOIN `images` AS i
+          ON p.`id` = i.`product_id`
+      ORDER BY p.id
+    ";
+
+
+
+$result = mysqli_query($conn, $query);
+
+$data = [];
+
+while($row = mysqli_fetch_assoc($result)){
+    $currentID = $row['id'];
+    $image = $row['image'];
+    if( isset($data[$currentID])) {
+        //$data[$row[`id`]][`images`][] = [$row[`images`]];
+        $data[$currentID]['image'][] = $image;
+    } else {
+        unset($row['image']);
+        $row['image'] = [];
+        $row['image'][] = $image;
+        //$data[$row[`id`]][`images`] = [$row[`images`]];
+        $data[$currentID] = $row;
+    }
+}
+
+$output = [
+    'success'=>true,
+    'products'=> $data
+];
+
+//php JSON stringify
+$json_output = json_encode($output);
+
+print($json_output);
+
+?>
---
+++ public/api/mysqlconnect.php
@@ -0,0 +1,9 @@
+<?php
+
+/* procedural style */
+    $conn = mysqli_connect('localhost','root','','c2.19wickedsales', 3306);
+
+/* oop style */
+    //$db = new mysqli('localhost','root','','c219_wickedsales', 3306);
+
+?>
