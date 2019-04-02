96964665eb2009a5233707a87f4732456e0019d6
Caroline Pan <abc1588@yahoo.com>
2019-04-02 15:52:44 -0700

    Routing setup in progress

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/maaterialize.min';
 import '../assets/css/app.css';
 import React from 'react';
-import {Route} from react-router-dom;
+import {Route} from 'react-router-dom';
 import ProductRoutes from './products';
 
 
--- src/components/products/product_details.js
+++ src/components/products/product_details.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-class ProductDetails extends Component (
+class ProductDetails extends Component {
     render() {
         return (
-            < div class = "product-details">
-                <h1> className = "center" >[Product Name]</h1>
-            < /div>
+            < div className = "product-details">
+                <h1 className = "center" >[Product Name]</h1>
+            </div>
         )
     }
-)
+}
 
 
