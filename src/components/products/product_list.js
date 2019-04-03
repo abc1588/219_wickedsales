a72e74dc31b0aa6b21ce2062878561a7b186f1bd
Caroline Pan <abc1588@yahoo.com>
2019-04-02 18:24:25 -0700

    added navigation menu and side nav bar

--- src/components/products/product_list.js
+++ src/components/products/product_list.js
@@ -4,8 +4,10 @@ class ProductList extends Component {
     render() {
         return (
             <div className = "product-list">
-                <h1> className="center" >Wicked Product List</h1>
+                <h1 className="center">Wicked Product List</h1>
             </div>
         )
     }
 }
+
+export default ProductList;
