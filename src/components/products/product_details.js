7529fa743abd48f1b59737411048aa570e2ba89f
Caroline Pan <abc1588@yahoo.com>
2019-04-09 18:58:57 -0700

    add new function product_add.js, modified product_details.js

--- src/components/products/product_details.js
+++ src/components/products/product_details.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import ProductAdd from './product_add';
 import ProductCarousel from './product_carousel';
 import MiscDetails from './misc_details';
 import { formatMoney } from '../../helpers';
@@ -51,21 +52,7 @@ class ProductDetails extends Component {
                     <ProductCarousel images={images}/>
                     <div className="col s12 m8">
                         <div className="right-align product-price">{formatMoney(price)}</div>
-                        <div className="right-align add-to-cart">
-                            <span className="qty-container">
-                                <button className="btn btn-small btn-floating purple lighten-1">
-                                    <i className="material-icons">remove</i>
-                                </button>
-                                <span className="product-qty">1</span>
-                                <button className="btn btn-small btn-floating purple lighten-1">
-                                    <i className="material-icons">add</i>
-                                </button>
-                            </span>
-
-                            <button className="btn purple darken-2">
-                                <i className="material-icons">add_shopping_cart</i>
-                            </button>
-                        </div>
+                        <ProductAdd/>
                         <p>{description}</p>
                         <MiscDetails details={miscDetails}/>
                     </div>
