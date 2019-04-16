e3b3301be79a871b08679ec72c6576a85f94c5e7
Caroline Pan <abc1588@yahoo.com>
2019-04-15 18:05:40 -0700

    added user sign in form with React

--- src/components/account/sign_in/sign_in_form.js
+++ src/components/account/sign_in/sign_in_form.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
+import Input from '../../general/input';
 
 const SignInForm = props => {
+    console.log('Sign In Form Props:', props);
+    const { handleSubmit, signIn } = props;
 
     return (
-        <form>
-            <h1>Form Goes Here</h1>
+        <form onSubmit={handleSubmit(signIn)}>
+            <div className="row">
+                <Field id="email" name="email" component={Input} label="Email"/>
+            </div>
+            <div className="row">
+                <Field id="password" name="password" component={Input} type="password" label="Password"/>
+            </div>
+                <div className="row">
+                <div className="col s12 right-align">
+                    <button className="btn purple darken-2">Sign In</button>
+                </div>
+            </div>
         </form>
     );
 }
